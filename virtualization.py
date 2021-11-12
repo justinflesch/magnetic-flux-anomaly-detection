b8dbf3dfea93e1ff45d7e876e171c8ed3e681c2f
jflesch <justin.flesch@intel.com>
2021-11-12 11:29:06 -0800

    Added matplotlib to show data

--- virtualization.py
+++ virtualization.py
@@ -1,14 +1,22 @@
+# Required libraries.
 from nptdms import TdmsFile
+import numpy as np
+import pandas as pd
+
+# Bokeh plots for viewing data.
+import matplotlib.pyplot as plt
 
 def virtualize(filepath):
     tdms_file = TdmsFile.read(filepath)
 
-    # Example of reading tdms file data.
-    for group in tdms_file.groups():
-        group_name = group.name
-        for channel in group.channels():
-            channel = channel.name
-            print(group_name, channel)
+    df = tdms_file.as_dataframe()
+    print(df)
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(grp_data)
+    plt.show()
+
 
 if __name__ == "__main__":
     virtualize(r"C:\Users\jflesch\Capstone\magnetic-flux-anomaly-detection\Test Data\VAR3---12m-15d-20y-15h-11m_SO_SGeo.tdms")
