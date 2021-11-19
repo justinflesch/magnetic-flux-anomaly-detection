46881e69a4e3a5112383203262cc9af5ab67a1c1
jflesch <justin.flesch@intel.com>
2021-11-18 17:33:18 -0800

    Visualize individual sensor data

--- virtualization.py
+++ virtualization.py
@@ -7,15 +7,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def virtualize(filepath):
-    tdms_file = TdmsFile.read(filepath)
+    tdms_file = TdmsFile.read(filepath) # Read in tdms file.
 
-    df = tdms_file.as_dataframe()
-    print(df)
+    df = tdms_file.as_dataframe() # Convert tdms file to Pandas dataframe.
+    print(df['/\'Measurements\'/\'P16C10z\''])
 
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    ax.plot(grp_data)
-    plt.show()
+    # Resample the dataset by averages.
+    rollmean = df.mean()
+    rollstd = df.std()
+
+    plt.figure(1)
+
+    #print(df['/\'Measurements\'/\'Furnace Current\'']) # How to access a specific row in the dataframe.
+    #print(df['/\'Measurements\'/\'P16C10z\'']) 
+    
+    for col in df:
+        print(col)
+        if str(col)[17] == "P": # Columns that are sensor data have a P in the 17th index of the column name.
+            plt.subplot() # Add a subplot to our plot for this data.
+            #print(df[col]) # Print the data within this dataframe column.
+            plt.plot(df[col], label="Raw Data") # Plot this data.
+            plt.plot(df[col].mean(), color="black", label="Rolling Mean")
+            plt.plot(rollstd[col], color="red", label="Rolling Std")
+            plt.legend(loc='best')
+            plt.title(col)
+            plt.show() # Show this given plot.
+
+    # plt.show() # Show all data in one plot.
 
 
 if __name__ == "__main__":
