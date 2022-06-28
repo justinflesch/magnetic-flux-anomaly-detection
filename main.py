9a324c1c1351a993bbf4aefdbe4e67e317ccb1d3
BeniaminC <benjamin.condrea@gmail.com>
2022-06-28 16:44:51 -0700

    added ampere data utility import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_util.data_virtualization import virtualize
 from data_util.data_normalization import normalize
 from data_util.data_visualization import visualize
+from data_util.ampere_data import *
 
 if __name__ == "__main__":
     df = virtualize(r"C:\Users\fleschju\Documents\School\magnetic-flux-anomaly-detection\Test Data\VAR3---12m-15d-20y-14h-27m_SO_SGeo.tdms")
@@ -8,3 +9,5 @@ if __name__ == "__main__":
     normalized_df = normalize(df)
     
     visualize(normalized_df)
+
+    
